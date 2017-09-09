ce6d9e996b25be245ace94bc8ee044df5f2d6f51
Luxotus <luxotus@gmail.com>
2017-09-09 10:37:30 -0400

    moving functions to a better location

--- RasPi/Libraries/usefulFunctions.py
+++ RasPi/Libraries/usefulFunctions.py
@@ -37,3 +37,21 @@ def readAnalogDigitalConverter(adcnum, clockpin, mosipin, misopin, cspin):
     
 def celcius_to_farenheit(num):
     return num * 9.0 / 5.0 + 32.0
+    
+def read_temp_raw(device_file):
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = celcius_to_farenheit(temp_c)
+        return temp_f
