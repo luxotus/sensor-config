6585fec077ad0a28dd6ce16795d8b5393db72384
Luxotus <luxotus@gmail.com>
2017-09-09 11:13:42 -0400

    debugging

--- RasPi/Calibration/phSensor.py
+++ RasPi/Calibration/phSensor.py
@@ -2,11 +2,9 @@ import sys, os
 import time
 import RPi.GPIO as GPIO
 sys.path.insert(0, os.path.abspath('..'))
-from Libraries.usefulFunctions import readAnalogDigitalConverter
+from Libraries.usefulFunctions import *
 
-# MCP3008 Analog to Digital converter 
 GPIO.setmode(GPIO.BCM)
-DEBUG = 1
 
 # SPI port on the ADC to the Cobbler
 SPICLK = 18
@@ -20,17 +18,18 @@ GPIO.setup(SPIMISO, GPIO.IN)
 GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPICS, GPIO.OUT)
 
-# Analog pins starting with 0
-analogSensor_01 = 0; 
+# PH sensor connected to analog pin 0 on the MCP3008
+phAnalogValue = 0; 
 
 if __name__ == '__main__':
     try:
         while True:
             # read the analog pin
-            analogValue_01 = readAnalogDigitalConverter(analogSensor_01, SPICLK, SPIMOSI, SPIMISO, SPICS)
+            phAnalogValue = 0; 
+            phAnalogValue = readAnalogDigitalConverter(phAnalogValue, SPICLK, SPIMOSI, SPIMISO, SPICS)
             
-            print "analog value: ", analogValue_01 
+            print "PH Analog Value: ", phAnalogValue
+            # print "Temp: " + str(read_temp()) + "F"
             time.sleep(1)
-    # Stop on Ctrl+C and clean up
     except KeyboardInterrupt:
         GPIO.cleanup()
